3e656d7ac1373dee7f8ba4e2ca990ccc6d124d21
Boris Hadzic <hadzicboris@gmail.com>
2018-06-12 22:53:22 +0200

    Updated .gitignore file. Add new type of exception and new domain model.

---
+++ booking/src/main/java/rs/ftn/xws/booking/domain/AccomodationImage.java
@@ -0,0 +1,41 @@
+package rs.ftn.xws.booking.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AccomodationImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String url;
+
+	public AccomodationImage() {
+
+	}
+
+	public AccomodationImage(String url) {
+		this.url = url;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
