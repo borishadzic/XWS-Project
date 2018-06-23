bbee81cfc378f8dfce083d808defea58c0820b3c
KirinDarko <33583523+KirinDarko@users.noreply.github.com>
2018-06-23 17:57:48 +0200

    Add files via upload
    Admin front app

---
+++ AdminFront/AdminFront/DTOs/AdminCommentDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminFront.DTOs
+{
+    public class AdminCommentDTO
+    {
+        public long id { get; set; }
+        public string comment { get; set; }
+        public string agent { get; set; }
+
+        public AdminCommentDTO()
+        {
+
+        }
+
+        public AdminCommentDTO(long id, string comment, string agent)
+        {
+            this.id = id;
+            this.comment = comment;
+            this.agent = agent;
+        }
+
+    }
+}
